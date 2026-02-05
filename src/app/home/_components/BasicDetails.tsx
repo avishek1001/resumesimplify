f07ce34442fc3d44bbb0e7f076d6bcf81199510b
avishek1001 <1706avishek@gmail.com>
2026-02-05 21:36:53 +0530

    Second commit: Finished "Basic Details" UI section.

--- src/app/home/_components/BasicDetails.tsx
+++ src/app/home/_components/BasicDetails.tsx
@@ -1,3 +1,5 @@
+import './BasicDetails.css'
+
 export default function BasicDetails(){
     return(
         <div className="m-3">
@@ -5,10 +7,10 @@ export default function BasicDetails(){
             <input type="text" id="name" className="border w-full h-10 mb-3" />
 
             <label htmlFor="email" className="block mb-3 text-3xl">E-MAIL</label>
-            <input type="text" id="email" className="border w-full h-10 mb-3" />
+            <input type="email" id="email" className="border w-full h-10 mb-3" />
             
             <label htmlFor="phone" className="block mb-3 text-3xl">PHONE</label>
-            <input type="text" id="phone" className="border w-full h-10 mb-3" />
+            <input type="tel" inputMode="numeric" id="phone" className="border w-full h-10 mb-3" />
             
             <label htmlFor="linkedin" className="block mb-3 text-3xl">LINKEDIN</label>
             <input type="text" id="linkedin" className="border w-full h-10 mb-3" />
@@ -17,7 +19,7 @@ export default function BasicDetails(){
             <input type="text" id="github" className="border w-full h-10 mb-3" />
 
             <label htmlFor="summary" className="block mb-3 text-3xl">SUMMARY</label>
-            <textarea className="border w-full" name="" id="summary" placeholder="Write your summary..."></textarea>
+            <textarea className="border w-full h-64" name="" id="summary" placeholder="Write your summary..."></textarea>
         </div>
     )
 }
